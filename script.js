989f1f00036fd0ae5da952f1ff0bda30ceeb23a8
sassan <sassan.marouf@gmail.com>
2020-09-28 20:47:34 +0200

    .

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const grid = document.querySelector("#grid");
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
-        let row = document.createElement("div");
-        row.className = "row";
-        for (let j = 0; j < 16; j++) {
-            let box = document.createElement("div");
-            box.className = "box";
-            box.innerText = 1;
-            row.appendChild(box);
-        }
-        grid.appendChild(row);      
+function createGrid(columns, rows) {
+
+    //set values of css variables to argument values
+
+    grid.style.setProperty("--columns", columns);
+    grid.style.setProperty("--rows", rows);
+
+    //create div elements
+
+    for (let i = 0; i < (columns * rows); i++) {
+      let box = document.createElement("div");
+      box.className = "box";
+      box.innerText = "";
+      grid.appendChild(box);
     }
+
+    //add event listener to every .box
+
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((boxes) => {
+        boxes.addEventListener("mouseenter", () => {
+            boxes.style.backgroundColor = "black";
+        })
+    })
 }
 
-createGrid();
+createGrid(16, 16);
