cabf7235748386aaf677897f1ba31187bf4f62a8
sassan <sassan.marouf@gmail.com>
2020-10-02 11:15:51 +0200

    .

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 const grid = document.querySelector("#grid");
 const resetBtn = document.querySelector("#resetBtn");
+const magicMode = document.querySelector("#magicMode");
+
+//initial grid when page is loaded
 window.onload = createGrid(16,16);
+window.onload = addBlackColor();
 
 function createGrid(columns, rows) {
 
     //set values of css variables to argument values
-
     grid.style.setProperty("--columns", columns);
     grid.style.setProperty("--rows", rows);
 
     //create div elements
-
     for (let i = 0; i < (columns * rows); i++) {
       let box = document.createElement("div");
       box.className = "box";
       box.innerText = "";
       grid.appendChild(box);
     }
-
-    //add event listener to every .box
-
-    const boxesInit = document.querySelectorAll(".box");
-    boxesInit.forEach((boxesInit) => {
-        boxesInit.addEventListener("mouseenter", () => {
-            boxesInit.style.backgroundColor = "black";
-        })
-    })
 }
 
+//reset button
 resetBtn.addEventListener("click", () => {
     let userInput = prompt("Please enter the number of columns/rows for your new grid (e.g. 64 will create a 64x64 grid)");
     
@@ -36,4 +30,36 @@ while (grid.firstChild) {
 }
 
 createGrid(userInput, userInput);
+addBlackColor();
 })
+
+//add event listener for black background color to every .box
+function addBlackColor() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((boxes) => {
+        boxes.addEventListener("mouseenter", () => {
+            boxes.style.backgroundColor = "black";
+        })
+    })
+}
+
+//magic mode button
+magicMode.addEventListener("click", () => {
+    let userInput = prompt("Please enter the number of columns/rows for your new grid (e.g. 64 will create a 64x64 grid)");
+    
+while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+}
+
+createGrid(userInput, userInput);
+})
+
+//add event listener for random background color to every .box
+function addRandomColor() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((boxes) => {
+        boxes.addEventListener("mouseenter", () => {
+            boxes.style.backgroundColor = "black";
+        })
+    })
+}
